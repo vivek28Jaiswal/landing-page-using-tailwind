4958a53b32171f78ecf18608ae9f4ba1191ff50d
vivek jaiswal <vivekfr28@gmail.com>
2024-11-17 13:08:48 +0530

    modified

---
+++ script.js
@@ -0,0 +1,43 @@
+// var arr = [11, "lavanya", function(){}]
+// const [first,sec,third] = arr;
+
+
+// fatarrow function
+// var fat = ()=> {
+//     console.log("BigChilll")
+// }
+// fat();
+
+// map and filter dono hi arr pe chlte hai, in dono ka hi kaam hai arr par kuch perform karna and "aur ek naya array return karna!"
+// map
+// var arr = [10, 20, 30, 40, 50];
+// const store = arr.map(val => val+1
+// )
+// console.log(store)
+// var arr = [100,200,300,400,500];
+// store = arr.map(val => val+5)
+// console.log(store);
+
+// var state = [10,5,4,3,2,11,12,13,14,15,16,17,18];
+// let store = state.map(elem => elem>5 ? elem+10 : elem)
+// console.log(store)
+
+
+// let students = [
+//     {name: "Rahul", age: 18, gender: "Male", Rollno: 110026},
+//     {name: "Mohan", age: 20, gender: "Male", Rollno: 110036},
+//     {name: "Sam", age: 19, gender: "Female", Rollno: 110016},
+//     {name: "Aakriti", age: 21, gender: "Female", Rollno: 110066}
+// ]
+
+// const store = students.filter(elem => elem.Rollno === 110036)
+
+// let product = [
+//     {name: "charger", model: "apple charger", price: 2000},
+//     {name: "screeen", model: "protector", price: 1200},
+//     {name: "watch", model: "apple watch", price: 14999},
+//     {name: "phone", model: "iphone 15 pro", price: 120000},
+//     {name: "ipad", model: "apple ipad mini", price: 54999},
+// ]
+
+// const store = product.filter(elem => elem.price<5000)
